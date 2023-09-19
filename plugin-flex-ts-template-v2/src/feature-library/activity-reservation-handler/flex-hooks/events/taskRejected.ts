76c5a1b46cf144e6228b4a7dc83b1a0a035ca0bc
André Silva <andrejs86@gmail.com>
2023-09-19 12:45:05 -0300

    removing unused features

--- plugin-flex-ts-template-v2/src/feature-library/activity-reservation-handler/flex-hooks/events/taskRejected.ts
+++
@@ -1,9 +0,0 @@
-import * as Flex from '@twilio/flex-ui';
-
-import taskEndedHandler from '../../helpers/taskEndedHandler';
-import { FlexEvent } from '../../../../types/feature-loader';
-
-export const eventName = FlexEvent.taskRejected;
-export const eventHook = (_flex: typeof Flex, _manager: Flex.Manager, task: Flex.ITask) => {
-  taskEndedHandler(task, eventName);
-};
--- plugin-flex-ts-template-v2/src/feature-library/enhanced-crm-container/types/ServiceConfiguration.ts
+++
@@ -1,6 +0,0 @@
-export default interface EnhancedCRMContainerConfig {
-  enabled: boolean;
-  url: string;
-  should_display_url_when_no_tasks: boolean;
-  display_url_when_no_tasks: string;
-}
