fed5399b8130fce0fbd5168a6018d79eb37fe7e8
André Silva <andrejs86@gmail.com>
2023-09-19 14:34:13 -0300

    removing device manager, adding skills

--- plugin-flex-ts-template-v2/src/feature-library/device-manager/config.ts
+++
@@ -1,8 +0,0 @@
-import { getFeatureFlags } from '../../utils/configuration';
-import DeviceManagerConfig from './types/ServiceConfiguration';
-
-const { enabled = false } = (getFeatureFlags()?.features?.device_manager as DeviceManagerConfig) || {};
-
-export const isFeatureEnabled = () => {
-  return enabled;
-};
