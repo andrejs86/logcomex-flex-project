25bd00f95c4faae84582c8c4b3473ef8e3b7f71f
André Silva <andrejs86@gmail.com>
2023-10-23 12:45:48 -0300

    Adding rollbar logging to React test

--- plugin-flex-ts-template-v2/src/feature-library/whatsapp-sender/flex-hooks/ViewCollection.tsx
+++ plugin-flex-ts-template-v2/src/feature-library/whatsapp-sender/flex-hooks/ViewCollection.tsx
@@ -1,13 +1,30 @@
 import * as Flex from '@twilio/flex-ui';
+import { Provider } from '@rollbar/react';
 
 import { FlexComponent } from '../../../types/feature-loader';
 import WhatsappSenderView from '../custom-components/WhatsappSenderView';
 
 export const componentName = FlexComponent.ViewCollection;
 export const componentHook = function addWhatsappSenderView(flex: typeof Flex, manager: Flex.Manager) {
+  const rollbarConfig = {
+    accessToken: '675268f348b14824a35d3d23d3577115',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    payload: {
+      client: {
+        javascript: {
+          code_version: '1.0.0',
+          source_map_enabled: true,
+        },
+      },
+    },
+  };
+
   flex.ViewCollection.Content.add(
-    <Flex.View name="whatsapp-view" key="whatsapp-view">
-      <WhatsappSenderView key="co-whatsapp-view" manager={manager} />
-    </Flex.View>,
+    <Provider config={rollbarConfig}>
+      <Flex.View name="whatsapp-view" key="whatsapp-view">
+        <WhatsappSenderView key="co-whatsapp-view" manager={manager} />
+      </Flex.View>
+    </Provider>,
   );
 };
